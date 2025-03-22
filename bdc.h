a4b34a708d6cfa1609988d61a222f38695e3a81c
gduan <gduan@fandm.edu>
2025-03-21 23:54:52 -0400

    Final Edition

--- bdc.h
+++ bdc.h
@@ -9,3 +9,8 @@ using namespace std;
 // since the vectors used in this program are not very large.
 string vec_to_string(vector<int> v);
 vector<int> biggest_divisible_conglomerate(vector<int> input);
+vector<int> bdc_helper(vector<int> input);
+vector<int> longest_vector(vector<vector<int>> candidates);
+vector<int> combine(const vector<int> &L, const vector<int> &R);
+vector <int> sub_vec(vector <int> input, size_t j);
+int find_next_dividend_position(vector <int> input, int i);
